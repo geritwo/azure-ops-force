c41592d16bc3d9b8b786d7b477d7e412b2f3d086
Gergo Vamosi <geritwo@gmail.com>
2022-12-18 01:57:14 +0100

    Note request URL and payload sample

--- adf_test.py
+++ adf_test.py
@@ -6,6 +6,7 @@ from azure.mgmt.datafactory import DataFactoryManagementClient
 
 AZURE_RESOURCE_GROUP = "ImageDataMiner"
 AZURE_DATA_FACTORY_NAME = "FlickrFlipprADF"
+AZURE_SUBSCRIPTION_ID = os.getenv('AZURE_SUBSCRIPTION_ID')
 
 
 def get_linked_services(client):
@@ -20,7 +21,7 @@ def get_linked_services(client):
 def main():
     client = DataFactoryManagementClient(
         credential=DefaultAzureCredential(),
-        subscription_id=os.getenv('AZURE_SUBSCRIPTION_ID')
+        subscription_id=AZURE_SUBSCRIPTION_ID
     )
 
     linked_services_details = get_linked_services(client)
@@ -28,6 +29,17 @@ def main():
         s = str(l).split(', \'properties\':')[0].replace('\'', '\"') + "}"
         j = json.loads(s)
         print(j['name'])
+
+    test_endpoint = f"https://management.azure.com/subscriptions/{AZURE_SUBSCRIPTION_ID}/resourcegroups/" \
+                    f"{AZURE_RESOURCE_GROUP}/providers/Microsoft.DataFactory/factories/{AZURE_RESOURCE_GROUP}/" \
+                    f"testConnectivity?api-version=2018-06-01"
+
+    payload = '{"linkedService":' \
+              '{"type":"LinkedServiceReference",' \
+              '"referenceName":"AzureBlobStorage1"},' \
+              '"activityId":"3a966aae-b74f-4d71-a964-931cdf0e44e1",' \
+              '"tags":{},' \
+              '"userAgent":"Madrid"}'
 
 
 if __name__ == "__main__":
