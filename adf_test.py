8f5662208191fb1a76a9261794d1fa6b7f2b8e7e
Gergo Vamosi <geritwo@gmail.com>
2022-12-18 01:39:20 +0100

    Parse LinkedService info from response

--- adf_test.py
+++ adf_test.py
@@ -1,10 +1,20 @@
 import os
+import json
 
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.datafactory import DataFactoryManagementClient
 
-AZURE_RESOURCE_GROUP="ImageDataMiner"
-AZURE_DATA_FACTORY_NAME="FlickrFlipprADF"
+AZURE_RESOURCE_GROUP = "ImageDataMiner"
+AZURE_DATA_FACTORY_NAME = "FlickrFlipprADF"
+
+
+def get_linked_services(client):
+    linked_services = client.linked_services.list_by_factory(
+        resource_group_name=AZURE_RESOURCE_GROUP,
+        factory_name=AZURE_DATA_FACTORY_NAME,
+    )
+
+    return linked_services
 
 
 def main():
@@ -13,12 +23,11 @@ def main():
         subscription_id=os.getenv('AZURE_SUBSCRIPTION_ID')
     )
 
-    response = client.linked_services.list_by_factory(
-        resource_group_name=AZURE_RESOURCE_GROUP,
-        factory_name=AZURE_DATA_FACTORY_NAME,
-    )
-    for item in response:
-        print(item)
+    linked_services_details = get_linked_services(client)
+    for l in linked_services_details:
+        s = str(l).split(', \'properties\':')[0].replace('\'', '\"') + "}"
+        j = json.loads(s)
+        print(j['name'])
 
 
 if __name__ == "__main__":
